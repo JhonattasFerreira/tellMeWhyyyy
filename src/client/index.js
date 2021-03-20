c4e4ddbe596c6f7660c0ac6db47a7714ebaeaded
Jhonattas Ferreira <jfs120@gmail.com>
2021-03-20 05:24:15 -0300

    feat: Create the index html and js

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import {handleSubmit} from './js/formHandler';
+import {validateUrl, getFormattedData} from './js/util';
+
+import './styles/main.scss';
+import './styles/base.scss';
+import './styles/form.scss';
+import './styles/error_message.scss';
+import './styles/content.scss';
+
+export {handleSubmit, validateUrl, getFormattedData}
